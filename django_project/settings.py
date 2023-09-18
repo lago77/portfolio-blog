c96ea45d71020b9a1c26126f510f8fe0ace5fb78
lago77 <lagomorph7777@gmail.com>
2023-09-18 19:53:21 +0000

    added requirements and changed settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-bf!u@@)#$m6u9ypow+kn8zfw(*wxb18e+iz%m^f2a9lh_0i%s%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['18.191.174.164']
+ALLOWED_HOSTS = ['3.141.201.6']
 
 #testing
 # Application definition
